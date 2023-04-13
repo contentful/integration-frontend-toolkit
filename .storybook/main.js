148fc9b5721ddffb499926416a1051ffeca6286b
George Chios <george.chios@contentful.com>
2023-04-13 15:23:11 -0600

    feat: switches from webpack5 over to vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,49 +1,35 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const path = require('path');
-// const { compilerOptions } = require('../tsconfig.json');
+const path = require("path");
+// const { compilerOptions } = require("../tsconfig.json");
+const tsconfigPaths = require("vite-tsconfig-paths");
 
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
-    "storybook-addon-designs",
     "@storybook/addon-a11y",
   ],
   typescript: {
     check: false,
     checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
+    reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
-    "include": ["src", "test"],
+    include: ["src", "test"],
   },
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
   staticDirs: ["../public"],
-  
-  webpackFinal: async (config, { configType }) => {
-    // config.resolve.modules = [
-    //   // path.resolve(__dirname, 'node_modules'),
-    //   path.resolve(__dirname, "src/")
-    // ]
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "helpers": path.resolve(__dirname, "../src/helpers") // how do broadly???
-    };
-
-    return config;
-  }
-  
 };
 
 export default config;
