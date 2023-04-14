6436decc75c4a1400942382513c8c6e1af7f71fa
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-14 16:22:28 -0600

    Added test coverage

--- src/components/general/EntityCard/EntityCard.tsx
+++ src/components/general/EntityCard/EntityCard.tsx
@@ -13,31 +13,26 @@ export interface Props {
 
 const EntityCard = (props: Props) => {
     const { title, description, entityType, contentType, loading, thumbnail } = props;
-    let card;
+    const card = title ?
+        <EntryCard contentType={contentType}
+            title={title}
+            description={description}
+            isLoading={loading}
+            thumbnailElement={<img alt="Thumbnail" src={thumbnail || Placeholder} />} />
+        :
+        <Card>
+            <Badge variant="negative" className={styles.missingBadge}>Deleted</Badge>
+            <Heading as="h2" data-test-id="title">
+                {entityType} Missing
+            </Heading>
+            <Paragraph>
+                This {entityType} is missing. It may have been deleted or moved.
+            </Paragraph>
+        </Card>
 
-    if (title) {
-        card =
-            <EntryCard contentType={contentType}
-                title={title}
-                description={description}
-                isLoading={loading}
-                thumbnailElement={<img alt="Thumbnail" src={thumbnail || Placeholder} />} />
-    } else {
-        card =
-            <Card>
-                <Badge variant="negative" className={styles.missingBadge}>Deleted</Badge>
-                <Heading as="h2">
-                    {entityType} Missing
-                </Heading>
-                <Paragraph>
-                    This {entityType} is missing. It may have been deleted or moved.
-                </Paragraph>
-            </Card>
-    }
 
     return (
-        <div className={styles.root}>
-
+        <div className={styles.root} id='EntityCard'>
             {card}
         </div>
     );
