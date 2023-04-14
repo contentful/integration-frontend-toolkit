35bf9c1dc2df29c6d1016f378cc4a14af8cb9cb3
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-14 15:44:09 -0600

    Added placeholder image
    - Grabbed a placeholder from brand.contentful.com
    - Added vite/client to types array for static asset handling

--- src/components/stacks/e-commerce/EntityCard/EntityCard.tsx
+++ src/components/stacks/e-commerce/EntityCard/EntityCard.tsx
@@ -1,21 +1,24 @@
 import { EntryCard, Card } from '@contentful/f36-components';
 import { styles } from './EntityCard.styles';
+import Placeholder from './assets/placeholder.png';
 
 export interface Props {
     title?: string,
     description?: string,
     contentType: string,
     loading?: boolean;
+    thumbnail?: string;
 }
 
 const EntityCard = (props: Props) => {
-    const { title, description, contentType, loading } = props;
+    const { title, description, contentType, loading, thumbnail } = props;
     return (
         <div className={styles.root}>
             <EntryCard contentType={contentType}
                 title={title}
                 description={description}
-                isLoading={loading} />
+                isLoading={loading}
+                thumbnailElement={<img alt="Thumbnail" src={thumbnail || Placeholder} />} />
         </div>
     );
 }
