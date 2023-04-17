f457c4100d231a98be1e081ac329ff91f718a9a9
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-17 11:48:59 -0600

    Added blank EntityCardList

---
+++ src/components/general/EntityCardList/EntityCardList.tsx
@@ -0,0 +1,21 @@
+import { } from '@contentful/f36-components';
+import { styles } from './EntityCardList.styles';
+
+export interface Props {
+
+}
+
+
+const EntityCardList = (props: Props) => {
+    const { } = props;
+
+
+
+    return (
+        <div className={styles.root} id='EntityCardList'>
+            <></>
+        </div>
+    );
+}
+
+export default EntityCardList;
