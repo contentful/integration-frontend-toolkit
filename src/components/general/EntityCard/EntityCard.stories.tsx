1bb6bd1a5181dd5ac585e7bd4d47a4c3b838bf46
Lillian Bitner <lillian.bitner@contentful.com>
2023-04-17 13:58:31 -0600

    Add HyperLink component

--- src/components/general/EntityCard/EntityCard.stories.tsx
+++ src/components/general/EntityCard/EntityCard.stories.tsx
@@ -1,23 +1,24 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import EntityCard from './EntityCard';
 
 const defaultData = {
-    title: 'My Awesome Title',
-    description: 'My Awesome Description',
-    contentType: 'Blog'
-}
+  title: 'My Awesome Title',
+  description: 'My Awesome Description',
+  contentType: 'Blog',
+};
 
 const meta: Meta<typeof EntityCard> = {
-    title: 'General/EntityCard',
-    component: EntityCard,
-    decorators: [
-        (Story) => (
-            <div style={{ width: '300px' }}>
-                <Story />
-            </div>
-        ),
-    ],
+  title: 'General/EntityCard',
+  component: EntityCard,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '300px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 type Story = StoryObj<typeof EntityCard>;
@@ -25,32 +26,31 @@ type Story = StoryObj<typeof EntityCard>;
 export const EntityCardDefault: Story = {};
 
 EntityCardDefault.args = {
-    ...defaultData,
-    entityType: 'Entry',
-}
+  ...defaultData,
+  entityType: 'Entry',
+};
 
 export const EntityCardLoading: Story = {};
 
 EntityCardLoading.args = {
-    ...defaultData,
-    entityType: 'Product',
-    loading: true
-}
+  ...defaultData,
+  entityType: 'Product',
+  loading: true,
+};
 
 export const EntityCardMissing: Story = {};
 
 EntityCardMissing.args = {
-    entityType: 'Product'
-}
+  entityType: 'Product',
+};
 
 export const EntityCardWithThumbnail: Story = {};
 
 EntityCardWithThumbnail.args = {
-    ...defaultData,
-    entityType: 'Category',
-    thumbnail: 'https://fastly.picsum.photos/id/666/200/200.jpg?hmac=keuaJ7nJC5hkHIEyee1b6jdEMbHBZbozYQ9NClt2_lk'
-}
-
-
+  ...defaultData,
+  entityType: 'Category',
+  thumbnail:
+    'https://fastly.picsum.photos/id/666/200/200.jpg?hmac=keuaJ7nJC5hkHIEyee1b6jdEMbHBZbozYQ9NClt2_lk',
+};
 
 export default meta;
