625430454d5d2900670bbad6ec073f1adeffd547
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-24 11:29:16 -0600

    Added support for single select

--- src/components/general/EntityCardList/EntityCardList.tsx
+++ src/components/general/EntityCardList/EntityCardList.tsx
@@ -3,21 +3,35 @@ import { styles } from './EntityCardList.styles';
 import EntityCard from '../EntityCard/EntityCard';
 import { Entity } from '../Entity.types';
 
+const SELECT_TYPE = {
+    MULTIPLE: 'multiple',
+    SINGLE: 'single'
+}
+
 export interface Props<T> {
     entityType: string,
     error?: boolean,
     list: T[],
     loading?: boolean,
     selected: Entity[],
+    selectType: 'multiple' | 'single';
     onSelect?: (card: Entity) => void;
 }
 
 
 const EntityCardList = <T extends Entity>(props: Props<T>) => {
-    const { entityType, list, loading, error, selected, onSelect } = props;
+    const { entityType, list, loading, error, selected, selectType, onSelect } = props;
 
     const isSelected = (e: Entity): boolean => {
         return selected?.findIndex((entity) => entity.title === e.title) > -1
+    }
+
+    const isSelectable = (): boolean => {
+        if (selectType === SELECT_TYPE.SINGLE) {
+            return !selected[0].title;
+        } else {
+            return selectType === SELECT_TYPE.MULTIPLE
+        }
     }
 
     return (
@@ -31,7 +45,7 @@ const EntityCardList = <T extends Entity>(props: Props<T>) => {
                     key={index}
                     className={`${styles.entityCard} entity-card-${index}`}
                     data-test-id="entity-card"
-                    onClick={() => onSelect && onSelect(listItem)}
+                    onClick={() => isSelectable() ? onSelect && onSelect(listItem) : null}
                     isSelected={isSelected(listItem)}
                     loading={loading}
                     {...listItem} />
--- src/components/general/EntityForm/EntityForm.tsx
+++ src/components/general/EntityForm/EntityForm.tsx
@@ -6,19 +6,26 @@ import { Entity } from '../Entity.types';
 import { useState } from 'react';
 import { SearchParams } from 'components/stacks/common/SearchBar/SearchBar.types';
 
+const SELECT_TYPE = {
+    MULTIPLE: 'multiple',
+    SINGLE: 'single'
+}
+
 
 export interface Props<T> {
     entityType: string;
+    selectType: 'multiple' | 'single';
     list: T[];
-    onCancel?: () => void;
-    onSubmit?: () => void;
+    onCancel: () => void;
+    onSubmit: (selected: Entity | Entity[]) => void;
 }
 
 
 const EntityForm = <T extends Entity>(props: Props<T>) => {
-    const { entityType, list, onCancel, onSubmit } = props;
-    const [searchableList, setSearchableList] = useState(list)
-    const [selected, setSelected] = useState([] as Entity[]);
+    const { entityType, selectType, list, onCancel, onSubmit } = props;
+    const [searchableList, setSearchableList] = useState<Entity[]>(list)
+    const [selected, setSelected] = useState<Entity>({} as Entity);
+    const [selectedMultiple, setSelectedMultiple] = useState<Entity[]>([]);
     const [showSelected, setShowSelected] = useState(false)
     const onSearch = (searchQuery: SearchParams) => {
         const newList = searchQuery.searchValue ? searchableList.filter((entity: Entity) => {
@@ -28,29 +35,52 @@ const EntityForm = <T extends Entity>(props: Props<T>) => {
         setSearchableList(newList)
     }
 
+    const isMultiSelect = (): boolean => {
+        return selectType === SELECT_TYPE.MULTIPLE;
+    }
+
+    const isDisabled = (): boolean => {
+        return isMultiSelect() ? selectedMultiple.length === 0 : !selected.title
+    }
+
     const onSelect = (item: Entity) => {
-        if (selected.length === 0) {
-            setSelected([item])
-        } else {
-            const index = selected.findIndex((e: Entity) => e.title === item.title);
-            if (index > -1) {
-                const newList = selected.filter((e: Entity) => e.title !== item.title)
-                setSelected(newList)
+        if (isMultiSelect()) {
+
+            if (selectedMultiple.length === 0) {
+                setSelectedMultiple([item])
             } else {
-                setSelected([...selected, item])
+                const index = selectedMultiple.findIndex((e: Entity) => e.title === item.title);
+                if (index > -1) {
+                    const newList = selectedMultiple.filter((e: Entity) => e.title !== item.title)
+                    setSelectedMultiple(newList)
+                } else {
+                    setSelectedMultiple([...selectedMultiple, item])
+                }
             }
+        } else {
+            if (selected.title === item.title) {
+                setSelected({} as Entity)
+            } else {
+                setSelected(item);
+            }
+        }
+    }
+
+    const clearSelected = () => {
+        if (isMultiSelect()) {
+            setSelectedMultiple([])
+        } else {
+            setSelected({} as Entity)
         }
     }
 
     return (
         <div className={styles.root} id='EntityForm'>
 
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={() => onSubmit(isMultiSelect() ? selectedMultiple : selected)}>
                 <Flex className={styles.heading} alignItems='center' justifyContent='space-between'>
                     <Text id="search-input-helptext">Search for a {entityType}: </Text>
-                    <TextLink onClick={() => setShowSelected(!showSelected)}>
-                        {showSelected ? 'Show all' : `Show Selected (${selected.length})`}
-                    </TextLink>
+
                 </Flex>
                 <Flex className={styles.searchBox} data-test-id="search-bar">
                     <SearchBar
@@ -59,21 +89,36 @@ const EntityForm = <T extends Entity>(props: Props<T>) => {
                         searchTitle={`Search for ${entityType}s`}
                     />
                 </Flex>
+                <Flex alignItems='center' justifyContent='space-between'>
+                    <TextLink onClick={() => setShowSelected(!showSelected)}>
+                        {showSelected ? 'Show all' : `Show Selected (${selectedMultiple.length})`}
+                    </TextLink>
+                    <TextLink onClick={clearSelected}>
+                        Clear Selected
+                    </TextLink>
+                </Flex>
+                <Flex className={styles.searchResults}>
 
-                {showSelected &&
-                    <Flex className={styles.searchResults}>
-                        <EntityCardList entityType={entityType} list={selected} selected={selected} />
-                    </Flex>
-                }
-                {!showSelected &&
-                    <Flex className={styles.searchResults}>
-                        <EntityCardList entityType={entityType} list={searchableList} selected={selected} onSelect={onSelect} />
-                    </Flex>
-                }
+                    {showSelected &&
+                        <EntityCardList
+                            entityType={entityType}
+                            list={isMultiSelect() ? selectedMultiple : [selected]}
+                            selected={isMultiSelect() ? selectedMultiple : [selected]}
+                            selectType={selectType} />
+                    }
+                    {!showSelected &&
+                        <EntityCardList
+                            entityType={entityType}
+                            list={searchableList}
+                            selected={isMultiSelect() ? selectedMultiple : [selected]}
+                            onSelect={onSelect}
+                            selectType={selectType} />
 
+                    }
+                </Flex>
                 <Flex className={styles.actions} alignItems='center' justifyContent='flex-end'>
                     <Button onClick={onCancel}>Cancel</Button>
-                    <Button variant="primary" type="submit" isDisabled={!selected.length}>Select {entityType}s</Button>
+                    <Button variant="primary" type="submit" isDisabled={isDisabled()}>Select {entityType}s</Button>
                 </Flex>
             </Form>
         </div>
