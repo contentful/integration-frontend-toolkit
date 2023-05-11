a3ab3fd106edc9a6254b1750fec47ade5cb161bc
Lillian Bitner <lillian.bitner@contentful.com>
2023-05-11 13:43:09 -0600

    fix(build): trying again with new config

---
+++ dist/components/stacks/eCommerce/ProductPicker/EntityForm/EntityForm.stories.d.ts
@@ -0,0 +1,8 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import EntityForm from './EntityForm';
+declare const meta: Meta<typeof EntityForm>;
+type Story = StoryObj<typeof EntityForm>;
+export declare const EntityFormDefault: Story;
+export declare const EntityFormVariants: Story;
+export declare const EntityFormSingleSelect: Story;
+export default meta;
