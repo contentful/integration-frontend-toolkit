6d26e247b8c2a8153c61df59e48b50b1203fbe8d
George Chios <george.chios@contentful.com>
2023-05-18 09:18:07 -0600

    chore: adds dist to the gitignore

--- dist/components/stacks/analytics/LineChart/LineChart.styles.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const styles: {
-  root: string;
-};
-//# sourceMappingURL=LineChart.styles.d.ts.map
--- dist/components/stacks/eCommerce/ProductPicker/EntityForm/EntityForm.styles.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const styles: {
-  root: string;
-  heading: string;
-  searchBox: string;
-  controls: string;
-  searchResults: string;
-  actions: string;
-};
-//# sourceMappingURL=EntityForm.styles.d.ts.map
