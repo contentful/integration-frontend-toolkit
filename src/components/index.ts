0a34e730df342d9a1bf51e7242b1826ae0541741
Lillian Bitner <lillian.bitner@contentful.com>
2023-12-19 14:45:52 -0700

    chore: add submodules for importing many packages

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { HyperLink } from './general/HyperLink/index.js';
+export { LineChart } from './stacks/analytics/LineChart/index.js';
+export { SearchBar } from './general/SearchBar/index.js';
+export { LocalHostWarning } from './general/LocalHostWarning/index.js';
