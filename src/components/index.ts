caa018f96918f9d0111c7d3c500f7c41a83bb8a5
Lillian Bitner <lillian.bitner@contentful.com>
2023-12-26 14:04:30 -0700

    fix: try js extensions on index files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { HyperLink } from './general/HyperLink/index';
-export { LineChart } from './stacks/analytics/LineChart/index';
-export { SearchBar } from './general/SearchBar/index';
-export { LocalHostWarning } from './general/LocalHostWarning/index';
+export { HyperLink } from './general/HyperLink/index.js';
+export { LineChart } from './stacks/analytics/LineChart/index.js';
+export { SearchBar } from './general/SearchBar/index.js';
+export { LocalHostWarning } from './general/LocalHostWarning/index.js';
