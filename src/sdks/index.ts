82d33f1ea89559979899c1b4313c7a0ccd02a549
George Chios <george.chios@contentful.com>
2024-03-17 21:06:40 -0700

    feat: exports segment context and actions for the configSaved event

--- src/sdks/index.ts
+++ src/sdks/index.ts
@@ -1,2 +1,4 @@
 export { sentryMarketplaceAppsSDK } from './sentry/sentryMarketplaceAppsSDK';
 export { SegmentAnalyticsProvider } from './segment/SegmentMarketplaceProvider';
+export { SegmentAnalyticsContext } from './segment/segmentMarketplaceContext';
+export { Action as ConfigAction } from './segment/typewriter/segment';
